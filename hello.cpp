eb54ef60cf2ee0b104f8448bb334b8deeb280e01
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-02-15 10:15:58 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int A,i;
+    cout<<"enter the value of A"<<endl;
+    cin>>A;
+    for(i=0;i<=A;i++)
+    {
+       cout<<"hello"<<endl; 
+    }
+    return 0;
+}
