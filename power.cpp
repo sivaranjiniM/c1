7ce2d4ccd678b23b01dc70ef4bdc7fc55066b648
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-02-15 11:55:47 +0530

    Create power.cpp

---
+++ power.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main() 
+{
+    int n,exponent,res;
+    cout<<"enter the value of n,exponent"<<endl;
+    cin>>n>>exponent;
+    res=pow(n,exponent);
+    cout<<"result="<<res<<endl;
+}
+
+   
