540099e39e8c275816e4733dbf9d0f4d5db606bd
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-02-15 16:27:54 +0530

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,13 @@
+ #include<iostream>
+  using namespace std;
+  int main()
+  {
+      int n,i;
+      cout<<"enter the value of n";
+      cin>>n;
+      for(i=0;i<=5;i++)
+      {
+          cout<<n*i;
+          
+      }
+  }
