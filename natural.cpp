58fbbe91d0785898942d7ee548c30a8e83c23930
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-02-15 10:30:41 +0530

    Create natural.cpp

---
+++ natural.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int i,n,sum=0;
+    cout<<"enter intiger value :"<<endl;
+    cin>>n;
+    for(i=0;i<=n;i++)
+ {
+     sum=sum+i;
+ }
+cout<<" sum"<<sum<<endl;
+}
