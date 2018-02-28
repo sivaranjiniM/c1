ae4d252232aff9592b05a5938ff82e3f50e7b013
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-02-28 19:40:56 +0530

    Create fib.cpp

---
+++ fib.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n,  a=1, b = 1, nextTerm = 0;
+
+    cout << "Enter the number of terms: ";
+    cin >> n;
+
+    cout << "Fibonacci Series: ";
+
+    for (int i = 1; i <= n; ++i)
+    {
+        
+        if(i == 1)
+        {
+            cout << a << " ";
+            continue;
+        }
+        if(i == 2)
+        {
+            cout << b << " ";
+            continue;
+        }
+        nextTerm = a + b;
+        a = b;
+        b = nextTerm;
+        
+        cout << nextTerm << " ";
+    }
+    return 0;
+}
