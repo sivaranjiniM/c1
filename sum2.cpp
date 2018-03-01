57104b832cd069bc69c47bbf4d20576c151df53f
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-03-01 22:28:23 +0530

    Create sum2.cpp

---
+++ sum2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int a, b, c;
+   cout << "Enter two numbers \n";
+   cin >> a >> b;
+   c = a + b;
+   cout <<"Sum of the numbers:"<< c << endl;
+  return 0;
+}
