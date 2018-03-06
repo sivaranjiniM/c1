d7a6565ad2c7918d1c63ec331c6a19acb8d53a32
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-03-06 17:41:37 +0530

    Create triangle.cpp

---
+++ triangle.cpp
@@ -0,0 +1,14 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+    float bs,ht;
+    double area;
+    cout<<"enter the base width and hight of the  triangle:";
+    cin>>bs>>ht;
+    area=0.5*(bs*ht);
+    cout<<"area:"<<area;
+    return 0;
+    
+    
+}
