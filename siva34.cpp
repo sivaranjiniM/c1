0d6411817b390ac034f18c5aa85670bc8381f2e4
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-03-19 17:54:29 +0530

    Create siva34.cpp

---
+++ siva34.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    char a=65;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+    for(int i=1;i<n;i++)
+    {
+    a++;
+} 
+    cout<<a;
+    return 0;
+}
