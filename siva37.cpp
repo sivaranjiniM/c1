fb1ea333ab5a6edb433aa8a14ebfe57f3859fb12
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-03-20 17:28:10 +0530

    Create siva37.cpp

---
+++ siva37.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int a[10];
+   int i,j,count=0;
+	for(i=0;i<10;i++)
+	{
+	cin>>a[i];
+	}
+  for(i=0;i<10;i++)
+	{
+	for(j=0;j<10;j++)
+	{
+	if(a[i]==a[j])
+	{
+	count=count+1;
+	}
+	}
+	if(count==1)
+	{
+	cout<<a[i];
+	}
+	}
+	return 0;
+}
