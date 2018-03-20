7bc796e15f579d155faf8e3947c885a71f549fbc
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-03-20 17:29:02 +0530

    Update siva37.cpp

--- siva37.cpp
+++ siva37.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main() 
 {
 	int a[10];
-   int i,j,count=0;
+        int i,j,count=0;
 	for(i=0;i<10;i++)
 	{
 	cin>>a[i];
 	}
-  for(i=0;i<10;i++)
+        for(i=0;i<10;i++)
 	{
 	for(j=0;j<10;j++)
 	{
