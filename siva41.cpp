3115b24118111fa5ddfb140e31d1d5c218e171ea
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-03-22 17:29:59 +0530

    Create siva41.cpp

---
+++ siva41.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include<math.h>
+int main()
+{
+	int a,b,c;
+  cin>>a;
+	cin>>b;
+	c=sqrt(a);
+	if(b==c)
+	cout<<"yes";
+	else
+	cout<<"no";
+	return 0;
+	
+}
