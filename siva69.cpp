d52fa728d96af2ef3ba02455093f4a4818ad179b
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-05-24 10:35:56 +0530

    Create siva69.cpp

---
+++ siva69.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+ using namespace std;
+int main() 
+{
+	int n;
+	cout<<" enter the number from 1 to 10"<<endl;
+	cin>>n;
+	switch(n)
+	{
+		case 1:
+		cout<<"one"<<endl;
+		break;
+		case 2:
+		cout<<"two"<<endl;
+		break;
+		case 3:
+		cout<<"three"<<endl;
+		break;
+		case 4:
+		cout<<"four"<<endl;
+		break;
+		case 5:
+		cout<<"five"<<endl;
+		break;
+		case 6:
+		cout<<"six"<<endl;
+		break;
+		case 7:
+		cout<<"seven"<<endl;
+		break;
+		case 8:
+		cout<<"eight"<<endl;
+		break;
+		case 9:
+		cout<<"nine"<<endl;
+		break;
+		case 10:
+		cout<<"ten"<<endl;
+		break;
+		default:
+		cout<<" enter numbers from 1 to 10"<<endl;
+	}
+	return 0;
+}
