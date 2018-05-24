b8794ede7743d6582052b5fd559a68569bd7a4a4
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-05-24 09:39:19 +0530

    Create siva64.cpp

---
+++ siva64.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int N;
+ cout<<"enter number:"<<endl;
+ cin>>N;
+ if(N>=1)
+ {
+ if(N>=10)
+ cout<<"no"<<endl;
+ else
+ cout<<"yes"<<endl;
+ }
+return 0;
+}
