a1773299f26eb6ce3ecef8b5ae638853396f6933
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-05-24 16:07:19 +0530

    Create siva94.cpp

---
+++ siva94.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,i,flag=0;
+cin>>n;
+for(i=2;i<=n/2;i++)
+{
+if(n%i==0)
+{
+flag=1;
+break;
+}
+}
+if(flag==0)
+{
+cout<<"no";
+}
+else
+{
+cout<<"yes";
+}
+return 0;
+}
