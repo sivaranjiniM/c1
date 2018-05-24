c53db595230e171f29d206d61de60377a0ac2d51
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-05-24 14:55:00 +0530

    Create siva89.cpp

---
+++ siva89.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a=2,i,n[i];
+	cout<<"total number"<<endl;
+	cout<<"enter the numbers"<<endl;
+	for(i=0;i<=a;i++)
+	{
+  cin>>n[i];
+	}
+	for(i=0;i<=a;i++)
+	{
+	cout<<i<<" "<<n[i]<<'\n';
+	}
+	return 0;
+}
