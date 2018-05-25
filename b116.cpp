10b2aff029f948039434704f139d023d32f5453c
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-05-25 14:43:20 +0530

    Create b116.cpp

---
+++ b116.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int a,b;
+  cout<<"enter the a value:"<<endl;
+  cin>>a;
+  cout<<"enter the b value:"<<endl;
+  cin>>b;
+  cout<<a<<b;
+}
