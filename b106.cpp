53c8bc26990d317e9fea45992c04825b64ad7f2d
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-05-25 12:27:39 +0530

    Create b106.cpp

---
+++ b106.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    char A=65;
+    cout<<"enter the number:"<<endl;
+    cin>>n;
+    for(int i=1;i<n;i++)
+    {
+    A++;
+    } 
+    cout<<A;
+    return 0;
+}
