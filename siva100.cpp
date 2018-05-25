ecf93219693c4b7bc3e1fb417fb83b09c26321f0
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-05-25 09:22:14 +0530

    Create siva100.cpp

---
+++ siva100.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ char ch;
+ cout << "Enter a character"<<endl;
+ cin >> ch;
+ cout << "ASCII Value of " << ch << " is " << int(ch);
+ return 0;
+}
