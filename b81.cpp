d2356d4aa458a7ecc8dd0d2faed21dd5be40590b
sivaranjiniM <36472110+sivaranjiniM@users.noreply.github.com>
2018-05-25 10:34:16 +0530

    Create b81.cpp

---
+++ b81.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+      int a,b,c;
+      cout<<"Enter the two nos:"<<endl;
+      cin>>a>>b;
+      if(a<b)
+      {
+      c=b-a;
+      cout<<"The result is:"<<c;
+      }
+      else
+      {
+      cout<<"a is greater than b:"<<endl;
+      }
+    return 0;
+}
